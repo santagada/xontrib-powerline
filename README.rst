6081c1de0a1d64496ffe9ba190fdb51eb80df9b2
Leonardo Santagada <santagada@gmail.com>
2016-09-15 11:27:12 +0200

    move back to rst for readme and improve our setup.py

---
+++ README.rst
@@ -0,0 +1,35 @@
+Xontrib Powerline
+=================
+
+Powerline for Xonsh shell.
+
+Install
+-------
+
+To install this xontrib first download and install the python package:
+
+::
+
+    pip install xontrib-powerline
+
+And them load it on your ``.xonshrc``
+
+::
+
+    xontrib powerline
+
+read more on the `xontrib docs`_ and if you want to create your own on
+the `xontrib tutorial`_
+
+Credits
+-------
+
+This package was created by Leonardo Santagada with
+Cookiecutter_ and the
+`laerus/cookiecutter-xontrib`_
+project template.
+
+.. _`xontrib docs`: http://xon.sh/xontribs.html
+.. _`xontrib tutorial`: http://xon.sh/tutorial_xontrib.html
+.. _Cookiecutter: https://github.com/audreyr/cookiecutter
+.. _`laerus/cookiecutter-xontrib`: https://github.com/laerus/cookiecutter-xontrib
--- setup.py
+++ setup.py
@@ -1,13 +1,40 @@
 from setuptools import setup
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='xontrib-powerline',
     version='0.1.0',
+
+    description='Powerline for Xonsh shell',
+    long_description=long_description,
     url='https://github.com/santagada/xontrib-powerline',
-    license='MIT',
+
     author='Leonardo Santagada',
     author_email='santagada@gmail.com',
-    description='Powerline for Xonsh shell',
+
+    license='MIT',
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+
     packages=['xontrib'],
     package_dir={'xontrib': 'xontrib'},
     package_data={'xontrib': ['*.xsh']},
